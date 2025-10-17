0867933ab642a1132dd9681b682423bc77d9dd5c
Efu Kn <anastasia.kn982@gmail.com>
2025-10-17 11:14:17 +0400

    изменение комментInitial commit

---
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,19 @@
+﻿#include <stdio.h>
+#include <math.h>
+#include <Windows.h>
+
+void main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    float h; // ввод высоты
+    printf("Введите высоту в метрах: ");
+    scanf_s("%f", &h);
+
+    float g = 9.8; // ускорение свободного падения
+    float t = sqrt(2 * h / g); // вычисление времени по высоте
+
+    printf("Предмет падает с высоты %f метров\n", h);
+    printf("Он коснётся земли через %f секунд\n", t);
+}
